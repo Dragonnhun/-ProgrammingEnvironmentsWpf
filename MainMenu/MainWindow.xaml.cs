3fbbc605405c8b4732645e13b4ad61ac37cc4725
Ádám Jakab <adam.jakab@lombiq.com>
2022-01-23 02:10:48 +0100

    Adding .gitignore, solution file and MainMenu

---
+++ MainMenu/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace MainMenu
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShooterGame_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("ShooterGame.exe");
+            Application.Current.Shutdown();
+        }
+
+        private void RunnerGame_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("RunnerGame.exe");
+            Application.Current.Shutdown();
+        }
+    }
+}
